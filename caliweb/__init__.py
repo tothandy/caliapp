d8b305992d8954a2eb7b7f72a6175e5d0ec09653
Andy Toth <ondrej.toth@code.berlin>
2020-04-07 18:14:33 +0200

    created error handlers for 404, 403

--- caliweb/__init__.py
+++ caliweb/__init__.py
@@ -2,6 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-# importing and register Blueprint. 
+# importing and register Blueprint. Connecting to the main app.
 from caliweb.core.views import core
 app.register_blueprint(core)
+
+from caliweb.error_pages.handlers import error_pages
+app.register_blueprint(error_pages)
