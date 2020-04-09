ab9b866d1a57f04490e57c0311e64e12a78e695b
Andy Toth <ondrej.toth@code.berlin>
2020-04-09 12:04:13 +0200

    updated user views and forms

--- caliweb/users/views.py
+++ caliweb/users/views.py
@@ -53,3 +53,36 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('core.index'))
+
+# Account (update form)
+@users.route('/account', methods=['GET', 'POST'])
+@login_required
+def account():
+
+    form = UpdateUserForm()
+        if form.validate_on_submit():
+
+            if form.picture.data:
+                username = current_user.username
+                pic = add_profile_pic(form.picture.data, username)
+                current_user.profile_image = pic
+
+            current_user.username = form.username.data
+            current_user.email = form.email.data
+            db.session.commit()
+            flash('Account Updated!')
+            return redirect(url_for('users.account'))
+
+        elif request.method == "GET":
+            form.username.data = current_user.username
+            form.email.data = current_user.email
+
+        profile_image = url_for('static', filename= 'profile_pics/' +current_user.profile_image)
+        return render_template('account.html', profile_image=profile_image, form=form)
+
+    @users.route("/<username>")
+    def user_workouts(username):
+        page = request.args.get('page', 1, type=int)
+        user = User.query.filter_by(username=username).first_or_404()
+        own_workouts = Workout.query.filter_by(author=user).order_by(Workout.date.desc()).paginate(page=page, per_page=5)
+        return render_template('user_own_workouts.html', own_workouts=own_workouts, user=user)
