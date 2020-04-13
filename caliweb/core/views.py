ff31c0edc8bd4cb516f35c8ff48575a3b8b47b4a
Andy Toth <ondrej.toth@code.berlin>
2020-04-13 17:49:03 +0200

    added workout templates

--- caliweb/core/views.py
+++ caliweb/core/views.py
@@ -1,10 +1,13 @@
+from caliweb.models import Workout
 from flask import render_template, request, Blueprint
 
 core = Blueprint('core',__name__)
 
 @core.route('/')
 def index():
-    return render_template('index.html')
+    page = request.args.get('page', 1, type=int)
+    own_workouts = Workout.query.order_by(Workout.date.desc()).paginate(page=page, per_page=10)
+    return render_template('index.html', own_workouts=own_workouts)
 
 @core.route('/info')
 def info():
